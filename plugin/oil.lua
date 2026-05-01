af0b73eb3915d86809839efb17420b29d1d4b35b
jobin_nelson <jobinnelson369@gmail.com>
2026-05-01 19:17:59 +0530

    feat: add keymap for detail view in oil.nvim

--- plugin/oil.lua
+++ plugin/oil.lua
@@ -5,6 +5,7 @@ vim.pack.add({
   'https://github.com/nvim-tree/nvim-web-devicons',
 }, { confirm = false })
 
+local detail = false
 require('oil').setup({
   default_file_explorer = true,
   delete_to_trash = true,
@@ -13,6 +14,17 @@ require('oil').setup({
     ['gy'] = 'actions.yank_entry',
     ['<C-s>'] = { 'actions.select', opts = { vertical = true } },
     ['<C-x>'] = { 'actions.select', opts = { horizontal = true } },
+    ["gd"] = {
+      desc = "Toggle file detail view",
+      callback = function()
+        detail = not detail
+        if detail then
+          require("oil").set_columns({ "icon", "permissions", "size", "mtime" })
+        else
+          require("oil").set_columns({ "icon" })
+        end
+      end,
+    },
   },
   view_options = {
     natural_order = "fast",
